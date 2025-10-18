8c967ddd3a8483026254bfd4a0e0d43867394e2e
Adonis <bigirimanaadonis@gmail.com>
2025-10-18 13:58:46 +0300

    SDF

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,20 @@
-// create your App component here
+
+import React, { useEffect, useState } from "react";
+
+function App() {
+  const [imageUrl, setImageUrl] = useState(null);
+
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then((response) => response.json())
+      .then((data) => setImageUrl(data.message));
+  }, []);
+
+  return (
+    <div>
+      {imageUrl ? <img src={imageUrl} alt="A Random Dog" /> : <p>Loading...</p>}
+    </div>
+  );
+}
+
+export default App;
